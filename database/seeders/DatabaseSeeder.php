e30c60dea0108469f415c539156a36e7677d03f9
DevOxilive <sistemas@oxilive.com.mx>
2026-05-11 16:09:41 -0600

    FIX: Reorganizar la llamada a UsersSeeder en DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
-            UsersSeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
             EmpleadoSeeder::class,
-
+            UsersSeeder::class,
         ]);
     }
 }
